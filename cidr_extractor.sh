9b4ba0428311f5788186aff75f0404d8bc23a607
PentiumB <5298.avgan.5298@gmail.com>
2025-06-27 20:08:22 +0500

    Create cidr_extractor.sh
    new method

---
+++ cidr_extractor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+orgs=("cloudflare" "fastly")
+
+inputv4="GeoLite2-ASN-Blocks-IPv4.csv"
+inputv6="GeoLite2-ASN-Blocks-IPv6.csv"
+
+for org in "${orgs[@]}"; do
+    awk -F',' -v pattern="$org" 'tolower($3) ~ pattern { print $1 }' "$inputv4" > "./source/${org}.lst"
+    awk -F',' -v pattern="$org" 'tolower($3) ~ pattern { print $1 }' "$inputv6" >> "./source/${org}.lst"
+done
